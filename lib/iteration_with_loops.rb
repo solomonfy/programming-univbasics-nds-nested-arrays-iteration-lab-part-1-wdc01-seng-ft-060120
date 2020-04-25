e60150a1cc374e2f6259982e2442b3976d24acb8
SOLOMON YISMAW <fantahunsolomon@gmail.com>
2020-04-25 07:51:23 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ row_index = 0
 while row_index < find_even_values.count do
   element_index = 0
   while element_index < find_even_values[row_index].count do
-    p find_even_values[row_index][element_index].even
+    p find_even_values[row_index][element_index]/2
     element_index += 1
   end
   row_index += 1
