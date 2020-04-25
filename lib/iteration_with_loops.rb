75d272ca1206310bd1454f9442466456becb3041
SOLOMON YISMAW <fantahunsolomon@gmail.com>
2020-04-25 07:57:09 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,9 +5,9 @@ def find_even_values(src)
   #Array_Arrays_Integers = [ 
 
 row_index = 0
-while row_index < find_even_values.count do
+while row_index < src.count do
   element_index = 0
-  while element_index < find_even_values[row_index].count do
+  while element_index < src[row_index].count do
     p find_even_values[row_index][element_index].even?
     element_index += 1
   end
